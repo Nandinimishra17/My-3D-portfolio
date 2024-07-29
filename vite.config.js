e38de359c09c18af247c1e361a34ba91eb0b9c8a
Nandini Mishra <118956437+Nandinimishra17@users.noreply.github.com>
2024-07-29 17:53:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  base: '/My-3D-portfolio/', // Ensure this matches the path where your site is deployed
+});
+
