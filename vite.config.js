e437d563360d6cd772c829e6fcc9c96c040b8c5f
Nandini Mishra <118956437+Nandinimishra17@users.noreply.github.com>
2024-07-29 18:04:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +7,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: '/index.html',
+      output: {
+        // Configure manualChunks to split chunks
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Example: Extract React and ReactDOM into a vendor chunk
+          // Add more chunks based on your project's needs
+        },
+        chunkSizeWarningLimit: 500 // Adjust this if you want to change the warning threshold
+      }
     },
     outDir: 'dist',
     assetsDir: 'assets',
   },
   base: '/My-3D-portfolio/', // Ensure this matches the path where your site is deployed
 });
+
 
